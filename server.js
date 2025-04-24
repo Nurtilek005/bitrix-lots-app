cbbe0f04c7f5ce9561d5e2793b9e547d7d835515
Nurtilek005 <140827106+Nurtilek005@users.noreply.github.com>
2025-04-24 20:26:56 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Отдача всего содержимого папки public
+// Отдаём index.html
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Отдаём manifest.json вручную
+app.get('/manifest.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'manifest.json'));
 });
 
+// Запуск
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running at http://localhost:${PORT}`);
