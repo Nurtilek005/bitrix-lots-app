fab81e7003e9f8b5f89b23519b47bc0a64a64d6e
Nurtilek005 <140827106+Nurtilek005@users.noreply.github.com>
2025-04-24 20:23:06 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Раздаёт файлы из папки public/
-app.use(express.static('public'));
+// Отдача всего содержимого папки public
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Отдаёт HTML при открытии корня сайта
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Запускает сервер на порту 3000
-app.listen(3000, () => {
-  console.log('App running on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App running at http://localhost:${PORT}`);
 });
